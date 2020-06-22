2959127ca10425dbd8861912e5ebeb68ce4b082d
plugon <plugon@naver.com>
2020-06-22 23:04:34 +0900

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(  
+        mainAxisAlignment : MainAxisAlignment.spaceEvenly,
+        children : [  
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -40,15 +53,34 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = Container(  
+      padding : const EdgeInsets.all(32),
+      child : Text(  
+'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap:  true,
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout Demo'),
+          title: Text('sRim 주식 종목 분석기'),
         ),
-        body: Column(
+        body: ListView(
           children: [
+            Image.asset( 'images/lake.jpg' ,
+            width : 600,
+            height : 240,
+            fit : BoxFit.cover,),
             titleSection,
+            buttonSection,
+            textSection
           ],
         ),
       ),
@@ -57,7 +89,22 @@ class MyApp extends StatelessWidget {
 
   Column _buildButtonColumn(Color color, IconData icon, String label){
     return Column(  
-
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment : MainAxisAlignment.center,
+      children:[
+        Icon(icon, color:color),
+        Container(  
+          margin: const EdgeInsets.only(top:8),
+          child: Text(  
+            label,
+            style: TextStyle(  
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color:color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
